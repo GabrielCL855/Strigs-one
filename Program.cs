c26bf21ebc3685f4a516e25a0f9b393b3ab40836
Gabriel Cabral <G.Cabral>
2020-10-02 21:34:06 -0300

    Adiciona a resposta do excericio

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StringsC1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String NomeUsuario;
+
+            Console.WriteLine("olá. Digite o seu nome: ");
+            NomeUsuario = Console.ReadLine();
+
+            Console.WriteLine($"Olá.{NomeUsuario}");
+
+        
+            
+            
+
+
+
+
+           
+        }
+    }
+}
